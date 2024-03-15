fa49baa23feb74cf89ff4a368e8cd2c4dcf10552
Dheeraj <dheeraj_pant@fluxsin.org>
2024-03-15 11:44:13 +0530

    Dheeraj - Murge

---
+++ FinanceForecasting.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FinanceForecasting.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ FinanceForecasting.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FinanceForecasting.Domain
+{
+    public class Class1
+    {
+
+    }
+}
